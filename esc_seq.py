b555255f3d60844cffc26e06b44862759599948a
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-26 19:00:10 +0530

    esc_seq.py

---
+++ esc_seq.py
@@ -0,0 +1,4 @@
+print("line A\nline B\nline C")  #\n = used for changing line 
+print("name\t Prateek")          #\t = used for giving tab space
+print("this is  backslash\\")    #\\ = used to print single backslash
+print("hell\blo")
