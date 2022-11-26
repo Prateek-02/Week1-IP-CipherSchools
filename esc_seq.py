abde4a2199c84328b19432df685effc74eddc325
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-26 19:27:39 +0530

    esc_seq.py

--- esc_seq.py
+++ esc_seq.py
@@ -1,4 +1,21 @@
+# \" = "
+# \\ = \
+# \' = '
+# \\n = \n 
+# \\t = \t 
+ 
 print("line A\nline B\nline C")  #\n = used for changing line 
 print("name\t Prateek")          #\t = used for giving tab space
 print("this is  backslash\\")    #\\ = used to print single backslash
-print("hell\blo")
+print("hell\blo")                #\b = used to apply backspace   output = hello
+
+# exercise 1
+
+print("this is \\\\ double backslash")        #output = this is \\ double backslash
+print("these are /\\/\\/\\/\\ mountains")     #output = these are /\/\/\/\ mountains
+print("he is\t awesome")                      #output = he is    awesome
+print("\\\" \\n \\t \\\' ")                   #output = \" \n \t \'
+
+#exercise 2
+print(r"this is A \n this is B")
+
