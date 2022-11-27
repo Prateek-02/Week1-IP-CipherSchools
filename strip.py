84508832cd5a23f96cd854328801aba4c108df5f
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-27 22:48:10 +0530

    strip.py

---
+++ strip.py
@@ -0,0 +1,9 @@
+name = " Prat  eek  "
+dots = "............."
+print(name + dots)
+#lstrip() method
+print(name.lstrip() + dots)         #it will remove the spaces of left side
+#rstrip() method
+print(name.rstrip() + dots)         #it will remove the space of right side
+
+print(name.replace(" ",""))
