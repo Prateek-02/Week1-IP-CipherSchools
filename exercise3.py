bc84e8972e2a8757f1acc2328836bae895fdf544
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-28 15:29:44 +0530

    exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,6 @@
+username = input("enter your name: ")
+age = int(input("enter your age: "))
+if age >= 10 and (username[0] == "a" or username[0] == "A"):
+    print("you can watch coco")
+else:
+    print("you cannot watch coco")    
