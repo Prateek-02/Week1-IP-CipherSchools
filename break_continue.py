5181b9d236d7e12264f26fdf2226d41ef2c42995
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-29 18:51:59 +0530

    break_continue.py

---
+++ break_continue.py
@@ -0,0 +1,17 @@
+#break
+# 1 to 10 print
+for i in range(1,11):         # it will print from 1 to 10
+    if i == 5:
+        break                 # when the value of i = 5, the loop will stop
+    print(i)
+    
+    
+    
+#continue
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i) 
+    
+    
+    
